fd4d5a1860cc0da299c782282c5c99afb33547e1
Visvix <jmeowf@gmail.com>
2017-10-12 20:23:29 -0400

    numbers attempt

---
+++ jf/near_hundredjm.py
@@ -0,0 +1,20 @@
+"""Given an int n, return True if it is within 10 of 100 or 200.
+
+Note: abs(num) computes the absolute value of a number.
+
+near_hundred(93) => True
+near_hundred(90) => True
+near_hundred(89) => False
+"""
+
+
+def near_hundred(n):
+    n+10 > 100
+    return True
+    n+10 < 100
+    return False
+
+
+print near_hundred(93)
+print near_hundred(90)
+print near_hundred(89)
