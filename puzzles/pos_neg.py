3eef28720d791ab7335777783593eb20a1aac977
Hello World <mff1075@gmail.com>
2017-10-12 20:14:25 -0400

    Coded parameter for pos_neg that return true if one value is negative and one is positive or, if third parameter, "negative", is True then returns true if both numbers are negative

--- puzzles/pos_neg.py
+++ puzzles/pos_neg.py
@@ -7,9 +7,16 @@ pos_neg(-1, 1, False) => True
 pos_neg(-4, -5, True) => True
 """
 def pos_neg(a, b, negative):
-  # Your code here...
-  return  # ...and here.
-
+  if a<0 and b>0:
+    return "True" # ...and here.
+  if a>0 and b<0:
+    return "True"
+  if a>0 and b>0:
+    return "False"
+  if a<0 and b<0 and negative==True:
+    return "True"
+  if a<0 and b<0 and negative!=True:
+    return "False"
 
 print pos_neg(1, -1, False)
 print pos_neg(-1, 1, False)
