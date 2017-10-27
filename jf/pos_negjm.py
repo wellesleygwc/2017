01d7b1d46fe1d7438107af71c5670576900e97dc
training2 <jmeowf@gmail.com>
2017-10-26 20:28:49 -0400

    posneg

---
+++ jf/pos_negjm.py
@@ -0,0 +1,26 @@
+"""Given 2 int values, return True if one is negative and one is positive.
+
+Except if the parameter "negative" is True, then return True only if both are negative.
+
+pos_neg(1, -1, False) => True
+pos_neg(-1, 1, False) => True
+pos_neg(-4, -5, True) => True
+"""
+
+
+def pos_neg(a, b, negative):
+    if a*b >= 0:
+      return False
+        if negative
+
+    if a*b <= 0:
+      return True
+
+
+
+
+
+
+print pos_neg(1, -1, False)
+print pos_neg(-1, 1, False)
+print pos_neg(-4, -5, True)
