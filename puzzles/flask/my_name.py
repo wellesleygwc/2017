79fbb7182f06d537dee778b0c849279e97481eed
norrisboyd <norrisboyd@gmail.com>
2017-11-15 20:12:02 -0500

    Add my_name.py Flask puzzle.

---
+++ puzzles/flask/my_name.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+"""
+Copy this file to your directory, then modify it so that
+going to "/name" displays your name on the web page.
+"""
+@app.route('/')
+def bye():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
