b9399145bdaa4dc40b234604c3b5bf204d5dc6f3
Jade <jmeowf@gmail.com>
2017-11-30 19:19:45 -0500

    login

---
+++ jf/main2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# Imports
+from flask import Flask, render_template, redirect, url_for, request, session
+import os, sys
+from app import db
+
+app = Flask(__name__)
+
+HOME_PAGE = 'index.html'
+
+# Home page
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template(HOME_PAGE)
+
+# Login page. Until we have a login page, go to the home page instead.
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("updatename.html")
+
+# Start the application
+if __name__== "__main__":
+    db.create_db()
+    app.run(debug=True)
